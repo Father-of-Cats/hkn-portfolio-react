ee88134376be0aa9d02c3f3c37e1ff89dcc2169e
Hunter Nelson <huntknelson@gmail.com>
2022-04-10 21:57:53 -0500

    footer is done

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { GoMarkGithub } from 'react-icons/go'
+import { FaLinkedinIn,FaTwitch } from 'react-icons/fa'
+
 
 function Footer() {
   return (
@@ -7,10 +10,15 @@ function Footer() {
       <Container className='p-4' fluid>
         <section className='mb-4'>
 
-          <a className='btn btn-outline-light btn-floating-m-1' href='#'>
-            <img class="bi-github"></img>
+          <a className='btn btn-outline-light m-2' href='https://github.com/Father-of-Cats'>
+          <GoMarkGithub/>
           </a>
-
+          <a className='btn btn-outline-light m-2' href='www.linkedin.com/in/hunter-k-nelson'>
+            <FaLinkedinIn/>
+          </a>
+          <a className='btn btn-outline-light m-2' href='#'>
+            <FaTwitch/>
+          </a>
         </section>
       </Container>
     </footer>
