cdff366fb7cba7813476e7a63d3f5761fe972844
Hunter Nelson <huntknelson@gmail.com>
2022-04-16 20:29:50 -0500

    navbar cleaned

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,28 +7,31 @@ const Header = ({ currentPage,pageChanger }) => {
 
 
     return(
-      <Navbar bg="dark" variant="dark">
-        <Container>
-        <Navbar.Brand href="#About">Hunter Nelson</Navbar.Brand>
-          <Nav className="me-auto">
+      <Navbar bg="dark" variant="dark" expand='lg'>
+        <Container fluid>
+          <Navbar.Brand className='text-left mx-4'><h1>Hunter Nelson</h1></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav className="navbar">
 
-            <Nav.Item onClick={() => {pageChanger('About');}} className='btn btn-outline-light m-2'>
-              <GoPerson/> About
-            </Nav.Item>
+              <Nav.Item onClick={() => {pageChanger('About');}} className={currentPage ==='About'?'btn btn-light m-2':'btn btn-outline-light m-2'}>
+                <GoPerson/> About
+              </Nav.Item>
 
-            <Nav.Item onClick={() => {pageChanger('Projects');}} className='btn btn-outline-light m-2'>
-              <GoFile/> Projects
-            </Nav.Item> 
+              <Nav.Item onClick={() => {pageChanger('Projects');}} className={currentPage ==='Projects'?'btn btn-light m-2':'btn btn-outline-light m-2'}>
+                <GoFile/> Projects
+              </Nav.Item> 
 
-            <Nav.Item onClick={() => {pageChanger('Contact');}} className='btn btn-outline-light m-2'>
-              <GoMail/> Contact
-            </Nav.Item>
+              <Nav.Item onClick={() => {pageChanger('Contact');}} className={currentPage ==='Contact'?'btn btn-light m-2':'btn btn-outline-light m-2'}>
+                <GoMail/> Contact
+              </Nav.Item>
 
-            <Nav.Item onClick={() => {pageChanger('Resume');}} className='btn btn-outline-light m-2'>
-              <GoBriefcase/> Resume
-            </Nav.Item> 
+              <Nav.Item onClick={() => {pageChanger('Resume');}} className={currentPage ==='Resume'?'btn btn-light m-2':'btn btn-outline-light m-2'}>
+                <GoBriefcase/> Resume
+              </Nav.Item> 
 
-          </Nav>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     )
