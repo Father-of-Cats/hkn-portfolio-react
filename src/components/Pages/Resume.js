cad203928ddca29269dda2e4930c14d1cd421ef2
Hunter Nelson <huntknelson@gmail.com>
2022-04-17 00:47:52 -0500

    resume added

--- src/components/Pages/Resume.js
+++ src/components/Pages/Resume.js
@@ -3,9 +3,9 @@ import { Container } from 'react-bootstrap'
 
 function Resume() {
   return (
-    <Container fluid>
+    <Container>
       
-      <iframe src=''></iframe>
+      <iframe title='Hunter's Resume src='.\DOC\HN_Resume.pdf' allowFullScreen='true' width='100%' height="800px"></iframe>
     </Container>
   )
 }
